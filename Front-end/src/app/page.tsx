3609812d6b9c25a292f50f3f4ef4079bd9ebccbb
kaleabkindu <kkmichaelstarkk@gmail.com>
2023-12-19 21:53:12 +0300

    implement how it works section UI

--- Front-end/src/app/page.tsx
+++ Front-end/src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/landing-page/HeroSection";
+import HowItWorkSection from "@/components/landing-page/HowItWorkSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection/>
+      <HowItWorkSection/>
     </div>
   )
 }
--- Front-end/src/data/index.ts
+++ Front-end/src/data/index.ts
@@ -33,3 +33,30 @@ export const help_center = [
         protected:false
     },
 ]
+
+export const howitworks = [
+    {
+        index:1,
+        image:'step-1.png',
+        name:'Filter & Discover',
+        description:'Connect with wallet, discover, buy NFTs, sell your NFTs and earn money'
+    },
+    {
+        index:2,
+        image:'step-2.png',
+        name:'Connect Wallet',
+        description:'Connect with wallet, discover, buy NFTs, sell your NFTs and earn money'
+    },
+    {
+        index:3,
+        image:'step-3.png',
+        name:'Start Trading',
+        description:'Connect with wallet, discover, buy NFTs, sell your NFTs and earn money'
+    },
+    {
+        index:4,
+        image:'step-4.png',
+        name:'Earn Money',
+        description:'Connect with wallet, discover, buy NFTs, sell your NFTs and earn money'
+    },
+]
