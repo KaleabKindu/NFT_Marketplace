e7812fe235e0bd59a2fb77e88b05d22321ba6b8f
kaleabkindu <kkmichaelstarkk@gmail.com>
2023-12-21 12:53:26 +0300

    fix bug on top creator component UI

--- Front-end/src/components/landing-page/TopCreators.tsx
+++ Front-end/src/components/landing-page/TopCreators.tsx
@@ -3,6 +3,7 @@ import { TypographyH2, TypographyH4, TypographyP } from '../common/Typography'
 import { Card } from '../ui/card'
 import { Avatar } from '../common/Avatar'
 import { Button } from '../ui/button'
+import { Badge } from '../ui/badge'
 
 type Props = {}
 
@@ -27,23 +28,24 @@ const TopCreators = (props: Props) => {
 
 const Creator = (props: Props) => {
   return (
-    <Card className='flex flex-col justify-evenly items-center h-[20rem] bg-secondary'>
+    <Card className='relative flex flex-col justify-evenly items-center h-[20rem] bg-secondary'>
         <div className='relative overflow-clip w-full h-[55%] '>
           <Image className='object-cover rounded-t-lg' src='/landing-page/futuristic-blue.jpg' fill alt=''/>
         </div>
-        <div className='relative z-50 rounded-full bg-secondary -mt-12 p-4'>
+        <div className='relative z-40 rounded-full bg-secondary -mt-12 p-4'>
           <Avatar className='h-20 w-20'/>
         </div>
         <div className='flex gap-3 items-center justify-around w-full p-3'>
           <div>
             <TypographyH4 text='Bruce Banner'/>
             <div className='flex items-center gap-3'>
-                <TypographyP className='text-primary/80' text='Sales: '/>
-                <TypographyP text='34.5ETH'/>
+                <TypographyP className='font-semibold text-primary/80' text='Sales: '/>
+                <TypographyP className='font-semibold' text='34.5ETH'/>
             </div>
           </div>
           <Button className='text-md rounded-full'>Follow</Button>
         </div>
+        <Badge className='absolute top-5 left-5 text-md bg-accent text-accent-foreground'>2</Badge>
     </Card>
   )
 }
