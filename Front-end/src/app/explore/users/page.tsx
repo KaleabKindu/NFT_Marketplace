efe78aded6b425fcf9c62c3f679f033a40dd43f2
kaleabkindu <kkmichaelstarkk@gmail.com>
2023-12-26 05:14:58 +0300

    implement users page UI

--- Front-end/src/app/explore/users/page.tsx
+++ Front-end/src/app/explore/users/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next/types'
-
+import { SearchInput } from '@/components/collection/SearchFilter'
+import UsersList from '@/components/explore/users/UsersList'
 export const metadata: Metadata = {
     title: 'Explore All Users | NFT Marketplace',
   }
@@ -8,7 +9,10 @@ type Props = {}
 
 const Page = (props: Props) => {
   return (
-    <div>Users</div>
+    <div className='flex flex-col gap-10 mt-16'>
+      <SearchInput/>
+      <UsersList/>
+    </div>
   )
 }
 
