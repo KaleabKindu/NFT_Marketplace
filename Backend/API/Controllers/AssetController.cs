9dc2a881bc581ccc42395646fc42254bff4dfc3f
tedblackson <tewodroslm@gmail.com>
2023-12-30 14:47:27 +0300

    code refactor

--- Backend/API/Controllers/AssetController.cs
+++ Backend/API/Controllers/AssetController.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Net;
 using Application.Common.Responses;
-using Application.Features.Assets.CQRS.Command;
-using Application.Features.Assets.CQRS.Query;
+using Application.Features.Assets.Command;
+using Application.Features.Assets.Query;
 using Application.Features.Assets.Dtos;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace API.Controllers
 {
@@ -23,46 +24,37 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var result = await _mediator.Send(new GetAssetByIdQuery { Id = id });
 
-            return HandleResult(result);
+            return HandleResult(await _mediator.Send(new GetAssetByIdQuery { Id = id }));
         }
 
         [HttpGet("all")]
         public async Task<IActionResult> GetAll([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
-            var result = await _mediator.Send(new GetAllAssetQuery { PageNumber = pageNumber, PageSize = pageSize });
-            return HandleResult(result);
+            return HandleResult(await _mediator.Send(new GetAllAssetQuery { PageNumber = pageNumber, PageSize = pageSize }));
         }
 
         [HttpPost]
 
         public async Task<IActionResult> Post([FromForm] CreateAssetDto createAssetDto)
         {
-            var result = await _mediator.Send(new CreateAssetCommand { CreateAssetDto = createAssetDto });
 
-            return HandleResult(result);
+            return HandleResult( await _mediator.Send(new CreateAssetCommand { CreateAssetDto = createAssetDto }));
         }
 
         [HttpPut]
         public async Task<IActionResult> Update([FromBody] UpdateAssetDto updateAssetDto)
         {
-            var result = await _mediator.Send(new UpdateAssetCommand { UpdateAssetDto = updateAssetDto });
 
-            return HandleResult(result);
+            return HandleResult(await _mediator.Send(new UpdateAssetCommand { UpdateAssetDto = updateAssetDto }));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var result = await _mediator.Send(new DeleteAssetCommand { Id = id });
 
-            return HandleResult(result);
+            return HandleResult(await _mediator.Send(new DeleteAssetCommand { Id = id }));
         }
-
-
-
-
 
     }
 }
