b9fdabb285c28d126512e7c432f1aa746ee67364
gadisamenu <amegadbay@gmail.com>
2024-01-28 16:34:50 +0300

    implement buy asset endpoint

---
+++ Backend/Application/Features/Buys/Commands/BuyAssetCommand.cs
@@ -0,0 +1,54 @@
+using ErrorOr;
+using MediatR;
+using AutoMapper;
+using Application.Contracts.Persistance;
+using Application.Common.Responses;
+using Application.Features.Busy.Dtos;
+using Application.Common.Errors;
+using Application.Common.Exceptions;
+
+namespace Application.Features.Buys.Commands
+{
+    public class BuyAssetCommand : IRequest<ErrorOr<BaseResponse<Unit>>>
+    {
+        public BuyAssetDto BuyAsset { get; set; }
+        public string UserPublicAddress { get; set; }
+    }
+
+    public class BuyAssetCommandHandler
+        : IRequestHandler<BuyAssetCommand, ErrorOr<BaseResponse<Unit>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public BuyAssetCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ErrorOr<BaseResponse<Unit>>> Handle(
+            BuyAssetCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            if (!await _unitOfWork.UserRepository.PublicAddressExists(request.UserPublicAddress))
+                return ErrorFactory.BadRequestError("User","User not found");
+
+            var user = await _unitOfWork.UserRepository.CreateOrFetchUserAsync(request.UserPublicAddress);
+
+            var asset = await _unitOfWork.AssetRepository.GetByIdAsync(request.BuyAsset.AssetId);
+
+            asset.Owner = user;
+        
+    
+            if (await _unitOfWork.SaveAsync() == 0) 
+                throw new DbAccessException("Unable to save to database");
+            
+            return new BaseResponse<Unit>(){
+                Message="Asset transfered successfully",
+                Value=Unit.Value
+            };
+        }
+    }
+}
