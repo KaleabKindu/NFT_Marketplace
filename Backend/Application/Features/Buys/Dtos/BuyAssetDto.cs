d3b86ac16fe0d793fda990e6f0494d54ddab9508
gadisamenu <amegadbay@gmail.com>
2024-01-29 15:39:03 +0300

    buyasset price type

--- Backend/Application/Features/Buys/Dtos/BuyAssetDto.cs
+++ Backend/Application/Features/Buys/Dtos/BuyAssetDto.cs
@@ -4,7 +4,7 @@ namespace Application.Features.Busy.Dtos
     public class BuyAssetDto
     {
         public long AssetId { set; get;}
-        public double Price { set; get; }
+        public string Price { set; get; }
         public string Hash { set; get;}
     }
 }
