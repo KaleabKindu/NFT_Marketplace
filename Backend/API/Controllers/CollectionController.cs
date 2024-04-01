321785145a78b60f572824b74d6e82f18013ac6f
Amha <amhaznif@gmail.com>
2024-04-01 19:13:06 +0300

    added collection details fetching endpoint

--- Backend/API/Controllers/CollectionController.cs
+++ Backend/API/Controllers/CollectionController.cs
@@ -16,7 +16,16 @@ namespace API.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> GetAllCollections([FromQuery] string Creator, [FromQuery] string Query, [FromQuery] AssetCategory Category = default, [FromQuery] double MinVolume = 0, [FromQuery] double MaxVolume = 1000.0d, [FromQuery] string SortBy = "date_added", [FromQuery] int PageNumber = 1, [FromQuery] int PageSize = 10)
         {
-            return HandleResult(await Mediator.Send(new GetAllCollectionsQuery(){ Creator=Creator, Query=Query, Category=Category, MinVolume=MinVolume, MaxVolume=MaxVolume, SortBy=SortBy, PageNumber=PageNumber, PageSize=PageSize }));
+            return HandleResult(await Mediator.Send(new GetAllCollectionsQuery { Creator=Creator, Query=Query, Category=Category, MinVolume=MinVolume, MaxVolume=MaxVolume, SortBy=SortBy, PageNumber=PageNumber, PageSize=PageSize }));
         }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+
+            return HandleResult(await Mediator.Send(new GetCollectionDetailsQuery { Id = id }));
+        }
+        
     }
 }
