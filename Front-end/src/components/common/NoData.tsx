ceae7c61e69146aca0a28f6c84b42670171a6537
KaleabKindu <kkmichaelstarkk@gmail.com>
2024-04-04 12:27:53 +0300

    integrate assets page ui

--- Front-end/src/components/common/NoData.tsx
+++ Front-end/src/components/common/NoData.tsx
@@ -2,16 +2,16 @@ import { SlSocialDropbox } from "react-icons/sl";
 import { TypographySmall } from "./Typography";
 
 type Props = {
-    message?:string
-}
+  message?: string;
+};
 
-const NoData = ({ message = 'No data found'}: Props) => {
+const NoData = ({ message = "No data found" }: Props) => {
   return (
-    <div className='col-span-12 w-full h-72 flex flex-col gap-3 items-center justify-center'>
-       <SlSocialDropbox size={60} /> 
-       <TypographySmall text={message} />
+    <div className="col-span-12 w-full h-72 flex flex-col gap-3 items-center justify-center">
+      <SlSocialDropbox size={60} />
+      <TypographySmall text={message} />
     </div>
-  )
-}
+  );
+};
 
-export default NoData
+export default NoData;
