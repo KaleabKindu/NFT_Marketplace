349fc68b39025beae04f3a38fe9be61bd5284cc6
Amha <amhaznif@gmail.com>
2024-04-06 02:28:49 +0300

    minor change in collection repo

--- Backend/Application/Contracts/Persistence/ICollectionRepository.cs
+++ Backend/Application/Contracts/Persistence/ICollectionRepository.cs
@@ -5,8 +5,7 @@ using Domain.Collections;
 namespace Application.Contracts.Persistance
 {
     public interface ICollectionRepository:IRepository<Collection>{
-        Task<IEnumerable<Collection>> GetAllAsync(string CreatorAddress, string Query, AssetCategory Category, double MinVolume, double MaxVolume, string SortBy, int page=1, int limit=10);
-        Task<int> CountAsync(string CreatorAddress, string Query, AssetCategory Category, double MinVolume, double MaxVolume);
+        Task<Tuple<int, IEnumerable<Collection>>> GetAllAsync(string CreatorAddress, string Query, AssetCategory Category, double MinVolume, double MaxVolume, string SortBy, int page=1, int limit=10);
     }
 
 }
