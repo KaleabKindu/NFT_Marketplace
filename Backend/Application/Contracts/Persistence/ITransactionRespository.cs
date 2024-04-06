aa9658ed2f2b82803ebbba7ca3f3d7df62c36d6e
tibeb-dev <kidussintayehu60@gmail.com>
2024-04-06 09:37:07 +0300

    feat: added fetch top creators

--- Backend/Application/Contracts/Persistence/ITransactionRespository.cs
+++ Backend/Application/Contracts/Persistence/ITransactionRespository.cs
@@ -1,10 +1,13 @@
 using Application.Contracts.Persistance;
 using Domain.Transactions;
+using Domain;
 
 namespace Application.Contracts.Persistence
 {
     public interface ITransactionRepository:IRepository<Transaction>{
         Task<IEnumerable<Transaction>> GetAllTransactionAsync(int page=1, int limit=10, int assetId=0);
+
+        Task<IDictionary<AppUser, double>> GetCreatorSalesVolumeAsync(int page=1, int limit=10);
 
      }
 
--- Backend/Persistence/Repositories/Transaction.cs
+++ Backend/Persistence/Repositories/Transaction.cs
@@ -1,5 +1,6 @@
 using Application.Contracts.Persistence;
 using Domain.Transactions;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Repositories
@@ -31,6 +32,19 @@ namespace Persistence.Repositories
                     .Include(x => x.Buyer)
                     .Include(x => x.Seller)
                     .ToListAsync();
+        }
+
+        public async Task<IDictionary<AppUser, double>> GetCreatorSalesVolumeAsync(int page = 1, int limit = 10)
+        {
+            var transactions = await GetAllTransactionAsync(page, limit); 
+
+            var creatorSalesVolume = transactions
+                .Where(t => t.Status == TransactionStatus.Completed && t.Type == TransactionType.Sell)
+                .GroupBy(t => t.Seller)
+                .Select(g => new KeyValuePair<AppUser, double>(g.Key, g.Sum(t => t.Amount)))
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            return creatorSalesVolume;
         }
     }
 }
