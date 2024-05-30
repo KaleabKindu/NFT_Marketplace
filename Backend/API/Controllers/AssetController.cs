8518552c8548674ac33edd9653fa79f722ce2e1f
gadisamenu <amegadbay@gmail.com>
2024-05-30 19:37:36 +0300

    update mint endpoint

--- Backend/API/Controllers/AssetController.cs
+++ Backend/API/Controllers/AssetController.cs
@@ -11,83 +11,86 @@ using Application.Features.Categories.Queries;
 namespace API.Controllers
 {
 
-    
+
     public class AssetsController : BaseController
     {
-        public AssetsController(IUserAccessor userAccessor):base(userAccessor){}
+        public AssetsController(IUserAccessor userAccessor) : base(userAccessor) { }
 
         [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var userId =  _userAccessor.GetUserId();
+            var userId = _userAccessor.GetUserId();
             return HandleResult(await Mediator.Send(new GetAssetByIdQuery { Id = id, UserId = userId }));
         }
-        
+
 
         [AllowAnonymous]
         [HttpGet("all")]
         public async Task<IActionResult> GetAll(
             [FromQuery] string query,
-            [FromQuery] double min_price, 
-            [FromQuery] double max_price, 
-            [FromQuery] AssetCategory? category, 
-            [FromQuery] string sort_by, 
-            [FromQuery] string sale_type, 
-            [FromQuery] long? collectionId, 
-            [FromQuery] string creatorId, 
-            [FromQuery] int pageNumber = 1, 
+            [FromQuery] double min_price,
+            [FromQuery] double max_price,
+            [FromQuery] AssetCategory? category,
+            [FromQuery] string sort_by,
+            [FromQuery] string sale_type,
+            [FromQuery] long? collectionId,
+            [FromQuery] string creatorId,
+            [FromQuery] int pageNumber = 1,
             [FromQuery] int pageSize = 10
         )
         {
-            var userId =  _userAccessor.GetUserId();
+            var userId = _userAccessor.GetUserId();
 
-            return HandleResult(await Mediator.Send(new GetAllAssetQuery { 
-                    UserId = userId,
-                    Query = query,
-                    MinPrice = min_price,
-                    MaxPrice = max_price,
-                    Category = category,
-                    SortBy = sort_by,
-                    SaleType = sale_type,
-                    CollectionId = collectionId,
-                    CreatorId = creatorId,
-                    PageNumber = pageNumber, 
-                    PageSize = pageSize 
-                }));
+            return HandleResult(await Mediator.Send(new GetAllAssetQuery
+            {
+                UserId = userId,
+                Query = query,
+                MinPrice = min_price,
+                MaxPrice = max_price,
+                Category = category,
+                SortBy = sort_by,
+                SaleType = sale_type,
+                CollectionId = collectionId,
+                CreatorId = creatorId,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            }));
         }
-        
+
         [AllowAnonymous]
         [HttpGet("trending")]
         public async Task<IActionResult> GetTrending(
-            [FromQuery] int pageNumber = 1, 
+            [FromQuery] int pageNumber = 1,
             [FromQuery] int pageSize = 10
         )
         {
-            var userId =  _userAccessor.GetUserId();
+            var userId = _userAccessor.GetUserId();
 
-            return HandleResult(await Mediator.Send(new GetTrendingAssetQuery() { 
+            return HandleResult(await Mediator.Send(new GetTrendingAssetQuery()
+            {
                 UserId = userId,
-                PageNumber = pageNumber, 
-                PageSize = pageSize 
+                PageNumber = pageNumber,
+                PageSize = pageSize
             }));
         }
-        
+
 
         [AllowAnonymous]
         [HttpGet("open-auction")]
-        public async Task<IActionResult> Get ([FromQuery] int pageNumber = 1 , [FromQuery] int pageSize = 10){
-           
+        public async Task<IActionResult> Get([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+
             return HandleResult(await Mediator.Send(new GetAssetsWOpenAuctQuery { PageNumber = pageNumber, PageSize = pageSize }));
 
 
         }
 
         [HttpPost("mint")]
-        public async Task<IActionResult> Post([FromForm] CreateAssetDto createAssetDto)
+        public async Task<IActionResult> Post([FromBody] CreateAssetDto createAssetDto)
         {
 
-            return HandleResult( await Mediator.Send(new CreateAssetCommand { CreateAssetDto = createAssetDto , Address = _userAccessor.GetAddress() }));
+            return HandleResult(await Mediator.Send(new CreateAssetCommand { CreateAssetDto = createAssetDto, Address = _userAccessor.GetAddress() }));
         }
 
         [HttpPut]
@@ -100,12 +103,12 @@ namespace API.Controllers
         [HttpPut("toggle-like/{id}")]
         public async Task<IActionResult> ToggleLike(int id)
         {
-            var userId =  _userAccessor.GetUserId();
+            var userId = _userAccessor.GetUserId();
 
             if (userId == null)
                 return Unauthorized();
 
-            return HandleResult(await Mediator.Send(new ToggleLikeAssetCommand { Id = id, UserId = userId}));
+            return HandleResult(await Mediator.Send(new ToggleLikeAssetCommand { Id = id, UserId = userId }));
         }
 
         [HttpDelete("{id}")]
