cf41b82af3d1897ad0834346c84bb10d44533c56
tedblackson <tewodroslm@gmail.com>
2024-06-08 22:33:35 +0300

    Add logic for delete command

--- Backend/Application/Features/Assets/Command/DeleteAssetCommand.cs
+++ Backend/Application/Features/Assets/Command/DeleteAssetCommand.cs
@@ -3,12 +3,14 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Application.Features.Assets.Dtos;
 using Application.Contracts.Persistance;
+using Application.Common.Errors;
 
 namespace Application.Features.Assets.Commands
 {
     public class DeleteAssetCommand : IRequest<ErrorOr<bool>>
     {
         public DeleteAssetEventDto _event;
+        public string Address { get; set; }
     }
 
     public class DeleteAssetCommandHandler
@@ -28,7 +30,21 @@ namespace Application.Features.Assets.Commands
             CancellationToken cancellationToken
         )
         {
+
+            var asset = await _unitOfWork.AssetRepository.GetByIdAsync(((long)command._event.TokenId));
+            var user = await _unitOfWork.UserRepository.GetUserByAddress(command.Address);
+
+            if (asset == null)
+                return ErrorFactory.NotFound("Asset", "Asset not found");
+            
+            if (user == null)
+                return ErrorFactory.NotFound("User", "User not found");
+            
+            if (asset.Owner.Address != command.Address)
+                return ErrorFactory.AuthorizationError("Asset", "User is not the owner of the asset");
+            
             _logger.LogInformation($"\nDeleteAssetEvent\nTokenID: {command._event.TokenId}\n");
+
             return true;
         }
     }
