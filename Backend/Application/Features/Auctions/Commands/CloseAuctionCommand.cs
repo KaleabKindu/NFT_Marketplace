b8e1935328f88e1f10b2b628b089ae66e849e659
tedblackson <tewodroslm@gmail.com>
2024-06-09 00:47:34 +0300

    Add close auction logic

--- Backend/Application/Features/Auctions/Commands/CloseAuctionCommand.cs
+++ Backend/Application/Features/Auctions/Commands/CloseAuctionCommand.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Application.Contracts.Persistance;
 using Application.Features.Auctions.Dtos;
+using Application.Common.Errors;
 
 namespace Application.Features.Auctions.Commands
 {
@@ -28,6 +29,35 @@ namespace Application.Features.Auctions.Commands
             CancellationToken cancellationToken
         )
         {
+
+            var auction = await _unitOfWork.AuctionRepository.GetByIdAsync((long)command._event.AuctionId);
+
+            if (auction == null)
+                return ErrorFactory.NotFound("Auction", "Auction not found");
+
+            var asset = await _unitOfWork.AssetRepository.GetAssetByTokenId(auction.TokenId);
+
+            if (asset == null)
+                return ErrorFactory.NotFound("Asset", "Asset not found");
+
+            var collection = await _unitOfWork.CollectionRepository.GetByIdAsync((long)asset.CollectionId);
+
+            if (collection == null)
+                return ErrorFactory.NotFound("Collection", "Collection not found");
+
+
+            collection.LatestPrice = auction.HighestBid;
+        
+
+            _unitOfWork.CollectionRepository.UpdateAsync(collection);
+
+            if (await _unitOfWork.SaveAsync() == 0)
+                return ErrorFactory.InternalServerError("Auction", "Error closing auction");
+
+
+
+
+
             _logger.LogInformation($"\nCloseAuctionEvent\nAuctionId: {command._event.AuctionId}\nWinner: {command._event.Winner}");
             return true;
         }
