957a922abf0c45da06163f7c77586144d816b87c
gadisamenu <amegadbay@gmail.com>
2026-02-12 15:12:53 +0300

    fix: add appSetting.json loading to db context factory

--- Backend/Persistence/AppDbContextFactory.cs
+++ Backend/Persistence/AppDbContextFactory.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Persistence
 {
-    public class AppDbContextFactory
+    public class AppDbContextFactory:IDesignTimeDbContextFactory<AppDbContext>
     {
         public AppDbContext CreateDbContext(string[] args)
         {
  
             var configuration = new ConfigurationBuilder()
-              .AddEnvironmentVariables("NFT_MARKET_")
-              .Build();
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables("NFT_MARKET_")
+                .Build();
+
 
             var builder = new DbContextOptionsBuilder<AppDbContext>();
-            string connectionString = configuration.GetConnectionString("AppConnectionString");
+            string connectionString = configuration["PostgreSQL:ConnectionString"];
             builder.UseNpgsql(connectionString);
 
             return new AppDbContext(builder.Options);
